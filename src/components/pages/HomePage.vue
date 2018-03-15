9569240598673d64081e36181ec2fbbfe4e2afd7
Eronne <erwann.letue@gmail.com>
2018-03-15 17:11:25 +0100

    First commit

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>Chatbots</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-page'
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
